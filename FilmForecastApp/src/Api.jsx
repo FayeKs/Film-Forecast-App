767ba4b1ea5cecf6c2cfdc5b7bcb629881830eed
Faye Bishop <fayebishop44@gmail.com>
2025-03-22 13:31:47 -0500

    fix weather api functions

--- FilmForecastApp/src/Api.jsx
+++ FilmForecastApp/src/Api.jsx
@@ -1,12 +1,18 @@
 import axios from 'axios';
 
 const weather_api_key = "0e1fbae0a04a1dc05e56dc3730439ebc";
+// const movies_api_key = "58e8c643229600fec6c901b6bc778677";
 
 export const getWeather = async (city) => {
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${weather_api_key}&units=imperial`;
-    const response = await axios.get(url);
+    const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${weather_api_key}&units=imperial`;
+    const response = await axios.get(weatherUrl);
 
     const temp = Math.floor(response.data.main.temp);
    
     return temp;
 }
+
+
+
+
+//function to fetch movie recommendations based on weather conditions
--- FilmForecastApp/src/components/WeatherDisplay/WeatherDisplay.jsx
+++ FilmForecastApp/src/components/WeatherDisplay/WeatherDisplay.jsx
@@ -1,45 +1,77 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react';
 import './WeatherDisplay.css'
+import axios from 'axios';
 import sunny from '../../assets/icons/sunny.png'
+import Clouds from '../../assets/icons/Clouds.png'
 import wind from '../../assets/icons/wind.png'
 import humidity from '../../assets/icons/humidity.png'
 import rain from '../../assets/icons/rain.png'
 import { CiSearch } from "react-icons/ci";
-import {getWeather} from '../../Api';
+
 
 
 const WeatherDisplay = () => {
-    const [city, setCity] = useState("");
-    const [weather, setWeather] = useState("");
- 
-    const fetchWeather = async () => {
-        const data = await getWeather(city);
-        setWeather(data);
-    }
+        const [data, setData] = useState({
+           fahrenheit: 10,
+           name: `London`,
+           humidty: 10,
+           wind: 12,
+           image: sunny
+        })
 
+const [name, setName] = useState('')
+
+
+        const handleClick = () => {
+            if (name !== "") {
+                const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${name}&appid=0e1fbae0a04a1dc05e56dc3730439ebc&units=imperial`
+                axios.get(apiUrl)
+                .then(res => {
+                    let imagePath = '';
+                    console.log(res.data);
+                    if (res.data.weather[0].main == "Clear") {
+                        imagePath = sunny;
+                    } else if(res.data.weather[0].main === "Rain") {
+                        imagePath = rain;
+                    } else if (res.data.weather[0].main === "Clouds"){
+                        imagePath = Clouds;
+                    } else if (res.data.weather[0].main === "Mist") {
+                        imagePath = Clouds;
+                    }
+
+                    setData({...data, fahrenheit: res.data.main.temp, name: res.data.name, humidity: res.data.main.humidity, image: imagePath, wind: res.data.wind.speed
+                        
+                    })
+                })
+                .catch( err => console.log(err));
+            }
+        }
+    
   return (
     <div className='weatherDisplay'>
         <input 
         type="text" 
         placeholder='Search City'
-        value={city} 
-        onChange={(e) => setCity(e.target.value)}
+        onChange={e => setName(e.target.value)}
         className='city-input'/>
-        <button onClick={fetchWeather}><CiSearch/></button>
+        <button onClick={handleClick}><CiSearch/></button>
         <div className='currentForecast'>
+            <h2>{data.name}</h2>
+            <p>{Math.round(data.fahrenheit)}°F</p>
+            <img src={data.image} alt="" />
+            <p></p>
         </div>
+
+
+
         <div className='currentWeather'>
             <li>
                 <img src={wind} alt="" />
-                <p>Wind</p>
-            </li>
-            <li>
-                <img src={rain} alt="" />
-                <p>precipitation</p>
+                <p>{Math.round(data.wind)}mph</p>
             </li>
             <li>
                 <img src={humidity} alt="" />
-                <p>humidity</p>
+                <p>{data.humidity}%</p>
             </li>
         </div>
     </div>
